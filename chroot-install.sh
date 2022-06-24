83c7ca8d2a10f75b5d7bcebedaa64136184e69ba
codelerk <swishyswashi@protonmail.com>
2022-06-24 09:56:23 -0400

    cleaned up script added options

--- chroot-install.sh
+++ chroot-install.sh
@@ -1,40 +1,77 @@
 #!/bin/bash
 
 # Timezone Setup and Lang Setup
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+echo "Enter Timezone (ex. America/New_York)"
+read -r timezone
+
+echo
+
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+install_pkgs() {
+	clear
+	echo "Enter Additional Packages: "
+	read -r pkgs
+
+	pacman -S $pkgs --noconfirm
+}
+
+install_pkgs
+
 # enable services
 systemctl enable NetworkManager
 systemctl enable dhcpcd
+systemctl enable sshd
 
 # User setup
 echo "Enter Username: "
-read uname
-useradd -mG wheel -s /usr/bin/fish $uname
-echo "Set $uname password: "
-passwd $uname
+read -r uname
+useradd -mG wheel $uname
 
-# root pass setup
-echo "Set Root password: "
-passwd root
+echo
+
+#set user password
+echo "Set $uname password: "; passwd $uname
+echo
+
+#set root passwd
+echo "Set Root password: "; passwd root
+echo
 
 # hostname setup
 echo "Set Hostname: "
-read hname
+read -r hname
 echo $hname > /etc/hostname
 
-# Grub Installer
-pacman -S grub efibootmgr
-grub-install /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+echo
 
-echo "done installing....now exit and umount -R /mnt"
+grub_install() {
+	echo "Enter the disk name (ex. /dev/sda): "
+	read -r drive_name
+	pacman -Syy grub efibootmgr --noconfirm
+	grub-install $drive_name
+	grub-mkconfig -o /boot/grub/grub.cfg
+}
 
+systemd_boot_install() {
+	bootctl install
+	echo -e "default arch.conf\ntimeout 10" > /boot/loader/loader.conf
+	echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=/dev/sda2 rw" > /boot/loader/entries/arch.conf
+}
 
-# Systemd-boot installer
-# echo -e "default arch.conf\ntimeout 10" > /boot/loader/loader.conf
-# echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=/dev/sda2 rw" > /boot/loader/entries/arch.conf
+# Choose your boot loader (Grub and Systemd boot are the only ones supported right now)
+
+echo -e "1 Grub Boot Loader\n2 Systemd-Boot Loader\n"
+echo "Choose Boot Loader: "
+read -r boot_loader
+
+case $boot_loader in
+	1) grub_install ;;
+	2) systemd_boot_install ;;
+	*) grub_install ;;
+esac
--- install.sh
+++ install.sh
@@ -2,14 +2,50 @@
 
 timedatectl set-ntp true
 
-mkfs.ext4 /dev/sda2
-mkfs.fat -F 32 /dev/sda1
+echo "Hi Welcome to the Arch Kiss Installer!!!"
 
-mount /dev/sda2 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
+echo
 
-pacstrap /mnt base base-devel linux linux-firmware linux-headers dhcpcd networkmanager fish --noconfirm
+echo "First thing we are going to do is partition the select disk we are installing Arch Linux onto!!!"
+
+echo
+
+lsblk
+
+echo
+
+echo "Enter Disk (ex. /dev/sda): "
+read -r disk_drive
+
+cfdisk $disk_drive
+
+echo "Enter Boot Partition (ex. /dev/sda1): "
+read -r boot_partition
+
+echo
+
+echo "Enter Root Partition (ex. /dev/sda2): "
+read -r root_partition
+
+echo 
+
+echo "Enter Swap Partition: "
+read -r swap_partition
+
+echo
+
+mkfs.ext4 $root_partition
+mkfs.fat -F 32 $boot_partition
+
+mount $root_partition /mnt
+mount --mkdir $boot_partition /mnt/boot
+
+pacstrap /mnt base base-devel linux linux-firmware linux-headers dhcpcd networkmanager vim neofetch git openssh --noconfirm
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash <(curl -s https://raw.githubusercontent.com/codelerk/arch-install/main/chroot-install.sh)
+wait arch-chroot /mnt /bin/bash <(curl -s https://raw.githubusercontent.com/codelerk/arch-kiss-installer/main/chroot-install.sh)
+
+umount -R /mnt
+
+reboot
