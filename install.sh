3d0bbea4020320eabcd7150e9131ebf805985318
codelerk <swishyswashi@protonmail.com>
2022-06-24 14:42:59 -0400

    updated readme & list drives again after cfdisk

--- install.sh
+++ install.sh
@@ -3,30 +3,29 @@
 timedatectl set-ntp true
 
 echo "Hi Welcome to the Arch Kiss Installer!!!"
-
 echo
 
 echo "First thing we are going to do is partition the select disk we are installing Arch Linux onto!!!"
-
 echo
 
 lsblk
-
 echo
 
 echo -n "Enter Disk (ex. /dev/sda): "
 read disk_drive
 
 cfdisk $disk_drive
+echo
+
+lsblk
+echo
 
 echo -n "Enter Boot Partition (ex. /dev/sda1): "
 read boot_partition
-
 echo
 
 echo -n "Enter Root Partition (ex. /dev/sda2): "
 read root_partition
-
 echo 
 
 echo -n "Enter Swap Partition: (Leave this one empty, if no swap partition): "
